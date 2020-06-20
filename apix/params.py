2ff92415d035d4a2dbafef4c15149ecba79fef64
lsongzhi <lsongzhi@163.com>
2020-06-20 22:29:09 +0800

    refactor: params

--- apix/params.py
+++ apix/params.py
@@ -1,307 +1,48 @@
-from enum import Enum
-from typing import Any
+from typing import Union, Generic, TypeVar, ClassVar, Dict, Any
 
-from pydantic.fields import FieldInfo
-
-
-class ParamTypes(Enum):
-    query = "query"
-    header = "header"
-    path = "path"
-    cookie = "cookie"
+T = TypeVar('T', covariant=True)
 
 
-class Param(FieldInfo):
-    in_: ParamTypes
+class _Param(str, Generic[T]):
+    _in: ClassVar[str]
 
-    def __init__(
-            self,
-            default: Any,
-            *,
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            deprecated: bool = None,
-            **extra: Any,
-    ):
-        self.deprecated = deprecated
-        super().__init__(
-            default,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            **extra,
-        )
+    def __class_getitem__(cls, params):
+        ty = Union[params]
+        ty.__param_type__ = cls._in
+        return ty
 
 
-class Path(Param):
-    in_ = ParamTypes.path
-
-    def __init__(
-            self,
-            default: Any,
-            *,
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            deprecated: bool = None,
-            **extra: Any,
-    ):
-        self.in_ = self.in_
-        super().__init__(
-            ...,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            deprecated=deprecated,
-            **extra,
-        )
+class Query(_Param):
+    _in = 'Query'
 
 
-class Query(Param):
-    in_ = ParamTypes.query
-
-    def __init__(
-            self,
-            default: Any,
-            *,
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            deprecated: bool = None,
-            **extra: Any,
-    ):
-        super().__init__(
-            default,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            deprecated=deprecated,
-            **extra,
-        )
+class Header(_Param):
+    _in = 'Header'
 
 
-class Header(Param):
-    in_ = ParamTypes.header
-
-    def __init__(
-            self,
-            default: Any,
-            *,
-            alias: str = None,
-            convert_underscores: bool = True,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            deprecated: bool = None,
-            **extra: Any,
-    ):
-        self.convert_underscores = convert_underscores
-        super().__init__(
-            default,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            deprecated=deprecated,
-            **extra,
-        )
+class Path(_Param):
+    _in = 'Path'
 
 
-class Cookie(Param):
-    in_ = ParamTypes.cookie
-
-    def __init__(
-            self,
-            default: Any,
-            *,
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            deprecated: bool = None,
-            **extra: Any,
-    ):
-        super().__init__(
-            default,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            deprecated=deprecated,
-            **extra,
-        )
+class Cookie(_Param):
+    _in = 'Cookie'
 
 
-class Body(FieldInfo):
-    def __init__(
-            self,
-            default: Any,
-            *,
-            embed: bool = False,
-            media_type: str = "application/json",
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            **extra: Any,
-    ):
-        self.embed = embed
-        self.media_type = media_type
-        super().__init__(
-            default,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            **extra,
-        )
+class _Body(Dict[str, Any], Generic[T]):
+    _in: ClassVar[str]
+
+    def __class_getitem__(cls, params):
+        ty = Union[params]
+        ty.__param_type__ = cls._in
+        return ty
 
 
-class Form(Body):
-    def __init__(
-            self,
-            default: Any,
-            *,
-            media_type: str = "application/x-www-form-urlencoded",
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            **extra: Any,
-    ):
-        super().__init__(
-            default,
-            embed=True,
-            media_type=media_type,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            **extra,
-        )
+class Body(_Body):
+    _in = 'Body'
 
 
-class File(Form):
-    def __init__(
-            self,
-            default: Any,
-            *,
-            media_type: str = "multipart/form-data",
-            alias: str = None,
-            title: str = None,
-            description: str = None,
-            gt: float = None,
-            ge: float = None,
-            lt: float = None,
-            le: float = None,
-            min_length: int = None,
-            max_length: int = None,
-            regex: str = None,
-            **extra: Any,
-    ):
-        super().__init__(
-            default,
-            media_type=media_type,
-            alias=alias,
-            title=title,
-            description=description,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            min_length=min_length,
-            max_length=max_length,
-            regex=regex,
-            **extra,
-        )
+class Form(_Body):
+    _in = 'Form'
+
+
+__all__ = ['Query', 'Header', 'Path', 'Cookie', 'Body', 'Form']
